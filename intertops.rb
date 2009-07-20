d5cb5280bedd274e3b526d723e4e3b40988bcae5
lordmalak <t-witte@web.de>
2009-07-20 11:50:28 +0200

    Einige Änderungen, funktioniert aber noch nicht :-(

--- intertops.rb
+++ intertops.rb
@@ -6,8 +6,8 @@ require 'helpers/urltools.rb'
 class IntertopsScraper
 	def initialize(sports)
 		@sports = sports
-		@sporturls = {
-			'Baseball/MLB' => "http://pda.intertops.com/German/selectbet.asp?id=&os=pRbtgge&sprtyp=BB&scrncd=SM&compno=1524"
+		@sportqueries = {
+			'Baseball/MLB' => "os=pRbtgge&sprtyp=BB&scrncd=SM&compno=1524"
 		}
 		@sportids = {
 			'Baseball/MLB' => 101,
@@ -15,23 +15,48 @@ class IntertopsScraper
 			'Football/NFL' => 301,
 		}
 		@reg_expr_uebersicht = {
-			'Baseball/MLB' => /<A href="getsmbettype.asp?id=&os=pRbtgge&sprtyp=BB&scrncd=SM&compno=1524&machno=1524&betno=(\d{6})">/
+			'Baseball/MLB' => /<A href="getsmbettype\.asp\?id=&os=pRbtgge&sprtyp=BB&scrncd=SM&compno=1524&machno=1524&betno=(\d{6})">/
 		}
 		@reg_expr_games = {
-			'Baseball/MLB' => /(.{3}) \(.{3,15}\)   \((.{3,8})\)<\/A><br>\r\n\r\n<A href="getsmbet.asp?id=&os=pRbtgge&sprtyp=BB&scrncd=SM&compno=1524&bettyp=SM&betno=......&optino=SM.">(.{3}) \(.{3,15}\)   \((.{3,8})\)<\/A><br>/
+			'Baseball/MLB' => /(.{3}) \(.{3,15}\)   \((.{3,8})\)<\/A><br>\r\r<A href="getsmbet\.asp\?id=&os=pRbtgge&sprtyp=BB&scrncd=SM&compno=1524&bettyp=SM&betno=......&optino=SM.">(.{3}) \(.{3,15}\)   \((.{3,8})\)<\/A><br>/
 		}
 		@games = {}
+		@teams = {
+			"ARI"=>"Arizona Diamondbacks",
+			"ATL"=>"Atlanta Braves",
+			"BAL"=>"Baltimore Orioles",
+			"BOS"=>"Boston Red Sox",
+			"CHC"=>"Chicago Cubs",
+			"CIN"=>"Cincinnati Reds",
+			"COL"=>"Colorado Rockies",
+			"CWS"=>"Chicago White Sox",
+			"HOU"=>"Houston Astros",
+			"LAA"=>"LAA Angels",
+			"LOS"=>"Los Angeles Dodgers",
+			"MIL"=>"Milwaukee Brevers",
+			"MIN"=>"Minnesota Twins",
+			"NYM"=>"New York Mets",
+			"NYY"=>"New York Yankees",
+			"OAK"=>"Oakland Athletics",
+			"PHI"=>"Philadelphia Phillies",
+			"PIT"=>"Pittsburgh Pirates",
+			"SDG"=>"San Diego Padres",
+			"SFO"=>"San Francisco Giants",
+			"STL"=>"St. Louis Cardinals",
+			"TAM"=>"Tampa Bay Rays",
+			"TEX"=>"Texas Rangers",
+			"WAS"=>"Washington Nationals"			
+		}
 	end
 
 	def get_odds()
 		@sports.each do |sport|
 			puts("Hole #{sport}-Daten von Intertops")
-			headers = {}
-			post_request(@sporturls[sport], "", headers) { |strings|
-puts(strings)
+
+			post_request("http://pda.intertops.com/German/selectbet.asp", @sportqueries[sport]) { |strings|
 				strings.scan(@reg_expr_uebersicht[sport]).each do |id|
 					text = ""
-					post_request("http://pda.intertops.com/German/smbet.asp?id=&os=pRbtgge&sprtyp=BB&scrncd=SM&compno=1524&betno=#{id[0]}&smcstype=none&bettyp=SM", "", headers) { |strings2|
+					post_request("http://pda.intertops.com/German/smbet.asp", @sportqueries[sport] + "betno=#{id[0]}&smcstype=none&bettyp=SM") { |strings2|
 						text = text + strings2
 					}
 #game: team1, odd1, team2, odd2
@@ -62,9 +87,9 @@ puts(strings)
 #					file.puts("<date>", ?, "</date>")
 #					file.puts("<time>", ?, "</time>")
 
-					file.puts("<team1 id=\"#{game[0]}\">", game[2], "</team1>")
+					file.puts("<team1 id=\"0\">", @teams[game[0]], "</team1>")
 					file.puts("<odd1>", game[1], "</odd1>")
-					file.puts("<team2 id=\"#{game[2]}\">", game[6], "</team2>")
+					file.puts("<team2 id=\"0\">", @teams[game[2]], "</team2>")
 					file.puts("<odd2>", game[3], "</odd2>")
 
 					file.puts("</game>")
